5f95f61871b4da5c674641ae101ba13473f68244
GCaras <gcaras0@gmail.com>
2019-10-16 23:37:28 -0400

    added styling and responsive design in styled components

--- src/App.js
+++ src/App.js
@@ -1,46 +1,105 @@
 import React from 'react';
 import styled from 'styled-components'
-import headshot from './img/headshot.png'
 import SplashPage from './SplashPage'
 
-const AppContainer = styled.div`
+const AppContainer = styled.body`
   display: flex;
+  flex-direction: column;
+  font-family: 'Varela', sans-serif;
+  height: calc(100vh - 10%);
+  margin: auto;
+  width: 100vw;
 `
 
 const StyledHeaderContainer = styled.div`
   align-items: center;
-  background-image: linear-gradient(slateblue, lightgreen);
-  border-right: 2px solid white;
+  background-color: darkcyan;
+  border-bottom: 2px solid black;
   color: white;
   display: flex;
-  flex-direction: column;
-  font-family: 'Orbitron', sans-serif;
-  font-weight: bolder;
-  height: 100vh;
-  justify-content: center;
-  text-shadow: 2px 2px darkgray;
-  min-width: 200px;
-  max-width: 250px;
-  padding: 20px;
+  justify-content: space-between;
+  padding: 10px 20px;
+  text-shadow: 2px 2px black;
   text-align: center;
+  @media (max-width: 375px) {
+    flex-direction: column;
+    padding: 10px;
+  }
 `
 
 const StyledHeader = styled.h1`
   color: white;
-  
-`
-
-const HeadshotNavImage = styled.img`
-  border: 5px solid white;
-  border-radius: 25%;
-  height: 150px;
-  width: auto;
+  font-size: 40px;
+  margin: 0px;
+  @media (max-width: 375px) {
+    display: none;
+  }
 `
 
 const StyledNav = styled.nav`
+  align-items: center;
   display: flex;
-  flex-direction: column;
-  justify-content: space-between;  
+  justify-content: space-between;
+  @media (max-width: 768px) {
+    justify-content: space-around;
+    margin: 5px;
+  }
+`
+
+const StyledNavItem = styled.div`
+  /* background-color: black; */
+  background-image: linear-gradient(black, slategray, black);
+  border: 2px solid white;
+  border-radius: 13%/30%;
+  box-shadow: 2px 2px darkslategray;
+  font-size: 30px;
+  margin: 0 10px;
+  padding: 3px 5px;
+  &:hover{
+    box-shadow: 5px 5px darkslategray;
+    transform:scale(1.05);
+    z-index:1;
+  }
+  @media (max-width: 768px) {
+    font-size: 20px;
+    padding: 0 10px;
+  }
+`
+
+const StyledSocialLinksContainer = styled.div`
+  display: flex;
+  @media (max-width: 768px) {
+    margin: 3px;
+  }
+`
+
+const StyledSocialLinks = styled.img`
+  height: 50px;
+  padding: 0 5px;
+  &:hover{
+    transform:scale(1.1);
+    z-index:1;
+  }
+  @media (max-width: 768px) {
+    height: 30px;
+    padding: 0 5px;
+  }
+`
+
+const StyledFooterContainer = styled.div`
+  align-items: center;
+  background-color: darkcyan;
+  border-top: 2px solid black;
+  color: white;
+  display: flex;
+  height: 90px;
+  justify-content: center;
+  text-shadow: 2px 2px black;
+  text-align: center;
+  @media (max-width: 768px) {
+    flex-direction: column;
+    padding: 0px;
+  }
 `
 
 
@@ -49,16 +108,20 @@ function App() {
     <div>
       <AppContainer>
         <StyledHeaderContainer>
-          <StyledHeader>George Caras</StyledHeader>
-          <HeadshotNavImage src={headshot}></HeadshotNavImage>
-          <p>I bring functional solutions featuring engaging interfaces to make everyone's job easier.</p>
+          <StyledHeader>GC</StyledHeader>
           <StyledNav>
-            <div>Resume</div>
-            <div>Projects</div>
+            <a href="#"><StyledNavItem>Resume</StyledNavItem></a>
+            <a href="#"><StyledNavItem>Projects</StyledNavItem></a>
           </StyledNav>
         </StyledHeaderContainer>
         <SplashPage />
       </AppContainer>
+      <StyledFooterContainer>
+        <StyledSocialLinksContainer>
+          <a href=""><StyledSocialLinks src="https://image.flaticon.com/icons/svg/254/254394.svg" /></a>
+          <a href=""><StyledSocialLinks src="https://image.flaticon.com/icons/svg/25/25657.svg" /></a>
+        </StyledSocialLinksContainer>
+      </StyledFooterContainer>
     </div>
   );
 }
--- src/SplashPage.js
+++ src/SplashPage.js
@@ -1,19 +1,33 @@
 import React from 'react'
+import headshot from './img/headshot.png'
 import styled from 'styled-components'
 
-const HeightTest = styled.div`
+const HeadshotImage = styled.img`
+  border: 5px solid white;
+  border-radius: 10%;
+  box-shadow: 5px 5px black;
+  height: 150px;
+  margin: 5px;
+  width: 130px;
+`
+
+const StyledMain = styled.section`
     align-items: center;
-    background-image: linear-gradient(slateblue, lightgreen);
-    height: 100vh;
-    margin: 0;
+    background-color: lightpink;
+    background: cover;
+    display: flex;
+    flex-direction: column;
+    height: calc(100vh - 184px);
+    padding: 20px 0 0 0;
 `
 
 function SplashPage () {
     return(
         <div>
-            <HeightTest>
-                <p>Community-oriented Full-Stack developer driven to create and implement the most efficient solutions that achieve company and team goals.</p>
-            </HeightTest>
+            <StyledMain>
+                <HeadshotImage src={headshot} />
+                <p>"I bring functional solutions featuring engaging interfaces to make everyone's job easier."</p>
+            </StyledMain>
         </div>
     )
 }
