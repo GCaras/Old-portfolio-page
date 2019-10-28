2748894dba93cc7c04e1e8349a2d2ed753254c36
GCaras <gcaras0@gmail.com>
2019-10-28 17:48:28 -0400

    Updates

---
+++ precache-manifest.22111690a0a114c078d0dd03296086b0.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd25301a762de6b68173ff1ae03355ec",
+    "url": "/GCaras.github.io/index.html"
+  },
+  {
+    "revision": "9f38384583c37a12f123",
+    "url": "/GCaras.github.io/static/css/main.843214bb.chunk.css"
+  },
+  {
+    "revision": "7865490f14fe2034a15d",
+    "url": "/GCaras.github.io/static/js/2.16fc81f4.chunk.js"
+  },
+  {
+    "revision": "9f38384583c37a12f123",
+    "url": "/GCaras.github.io/static/js/main.48b302cf.chunk.js"
+  },
+  {
+    "revision": "93491c85357f6acf6684",
+    "url": "/GCaras.github.io/static/js/runtime-main.a48a9022.js"
+  },
+  {
+    "revision": "2602e246841cb900ac105903e7ab1eac",
+    "url": "/GCaras.github.io/static/media/GeorgeCaras_Resume.2602e246.pdf"
+  },
+  {
+    "revision": "f563f426e37c4f5274d8c7570fac9800",
+    "url": "/GCaras.github.io/static/media/TCGthumb.f563f426.jpg"
+  },
+  {
+    "revision": "8f9613cbe085903564a4372d63eb6072",
+    "url": "/GCaras.github.io/static/media/YouDueThumb.8f9613cb.png"
+  },
+  {
+    "revision": "aa54d3537bb3da66da629d2e302f0c73",
+    "url": "/GCaras.github.io/static/media/headshot.aa54d353.png"
+  },
+  {
+    "revision": "10901a6ebd87baea8e2609977540cf8b",
+    "url": "/GCaras.github.io/static/media/videogame-trivia-thumbnail.10901a6e.png"
+  }
+]);
