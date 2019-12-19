fa847ce5245fbfe5df693e0e2b4a5d50546072e2
GCaras <gcaras0@gmail.com>
2019-12-18 23:30:20 -0500

    Updates

---
+++ precache-manifest.25970eee688062b92092d954ab3ff66c.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "512a9af9ef757698cb0a5fa2fdbda46d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "936bae203f1a76f7c584",
+    "url": "/static/css/main.b60d42b1.chunk.css"
+  },
+  {
+    "revision": "612bfa0806048425a8e4",
+    "url": "/static/js/2.c356bf86.chunk.js"
+  },
+  {
+    "revision": "936bae203f1a76f7c584",
+    "url": "/static/js/main.27013ad2.chunk.js"
+  },
+  {
+    "revision": "957a0fccbb513420b7c5",
+    "url": "/static/js/runtime-main.13fb335e.js"
+  },
+  {
+    "revision": "f563f426e37c4f5274d8c7570fac9800",
+    "url": "/static/media/TCGthumb.f563f426.jpg"
+  },
+  {
+    "revision": "8f9613cbe085903564a4372d63eb6072",
+    "url": "/static/media/YouDueThumb.8f9613cb.png"
+  },
+  {
+    "revision": "aa54d3537bb3da66da629d2e302f0c73",
+    "url": "/static/media/headshot.aa54d353.png"
+  },
+  {
+    "revision": "9eed603251f7d6d5f7f3673e1773c288",
+    "url": "/static/media/nodejs.9eed6032.svg"
+  },
+  {
+    "revision": "d8cf77403db1c802576acc37627ff00b",
+    "url": "/static/media/react-brands.d8cf7740.svg"
+  },
+  {
+    "revision": "10901a6ebd87baea8e2609977540cf8b",
+    "url": "/static/media/videogame-trivia-thumbnail.10901a6e.png"
+  }
+]);
